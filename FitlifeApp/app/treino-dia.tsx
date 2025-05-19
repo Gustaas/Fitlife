96dbe4eb98cd6c2ab0214fb418cfa6f53340d62a
denilsonbg <denilsongb@outlook.com>
2025-05-19 20:29:51 -0300

    Update treino-dia.tsx
    Alteraçao cor background.

--- FitlifeApp/app/treino-dia.tsx
+++ FitlifeApp/app/treino-dia.tsx
@@ -31,7 +31,7 @@ export default function TreinoDiaScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 20 },
+  container: { flex: 1, padding: 20, backgroundColor: "#fff" },
   title: { fontSize: 24, fontWeight: 'bold', marginBottom: 10, textAlign: 'center' },
   subTitle: { fontSize: 18, marginBottom: 20, textAlign: 'center' },
   card: {
