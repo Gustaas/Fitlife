29befbc07ed72e8b4f52b48ffc1b2b0727167a0c
denilsonbg <denilsongb@outlook.com>
2025-05-19 20:37:08 -0300

    Update semana-treinos.tsx
    alteraçao cor back ground

--- FitlifeApp/app/semana-treinos.tsx
+++ FitlifeApp/app/semana-treinos.tsx
@@ -74,7 +74,7 @@ export default function SemanaTreinosScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 20 },
+  container: { flex: 1, padding: 20, backgroundColor: "#fff" },
   title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20, textAlign: 'center' },
   card: {
     marginBottom: 20,
